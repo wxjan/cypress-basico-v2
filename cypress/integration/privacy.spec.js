385258471c88c2aa4fee74625fe92b006bf94a6b
Janaina Sposito <janainasilva.1993@outlook.com>
2024-10-15 16:14:21 -0300

    Corrigido teste de política de privacidade

--- cypress/integration/privacy.spec.js
+++ cypress/integration/privacy.spec.js
@@ -1,5 +1,5 @@
 it('testa a página da política de privacidade de forma independente', function(){
-  cy.visit('./src/index.html')
+  cy.visit('./src/privacy.html')
 
   cy.contains('Talking About Testing').should('be.visible')
 })
